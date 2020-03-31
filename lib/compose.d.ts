c04c1a7c5b78dd37ce8f80015b9e72672ab4c8fe
knilink <esurllink@gmail.com>
2020-03-31 17:14:36 +1100

    compile

--- lib/compose.d.ts
+++ lib/compose.d.ts
@@ -2,4 +2,4 @@ import React from 'react';
 export interface MiddlewareProps {
     next: (props: any) => React.ReactElement | null;
 }
-export default function compose<P extends MiddlewareProps>(widgets: React.ComponentType<P>[]): React.ComponentType<P>;
+export default function compose<P extends MiddlewareProps>(widgets: React.ComponentType<P>[]): React.FC<P>;
--- lib/utils.d.ts
+++ lib/utils.d.ts
@@ -1,33 +1,34 @@
 import React from 'react';
-import { JSONSchema7Definition } from 'json-schema';
+import { JSONSchema7Definition, JSONSchema7 } from 'json-schema';
 import { MiddlewareProps } from './compose';
+interface SchemaLocalRef {
+    schema: JSONSchema7Definition;
+    path: (string | number)[];
+}
 export interface FormMiddlewareProps<FP = {}> extends MiddlewareProps {
     schema: JSONSchema7Definition;
     parent: FormMiddlewareProps | null;
-    data: any;
+    data: unknown;
     onChange: Function;
     schemaPath: (string | number)[];
     dataPath: (string | number)[];
     MiddlewareComponent: React.ComponentType<FormMiddlewareProps>;
     formProps: FormProps & FP;
-}
-export interface UseAdditional {
-    onAdd: ((newData: any) => void) | null;
-    arrayBody: React.ReactElement[] | null;
-}
-export interface AdditionalItemTemplateProps extends FormMiddlewareProps {
-    onMove: (newIndex: number) => void;
+    localRefs?: {
+        [key: string]: SchemaLocalRef;
+    };
 }
 export interface FormProps {
-    schema: JSONSchema7Definition;
+    schema: JSONSchema7;
     middlewares: React.ComponentType<FormMiddlewareProps> | React.ComponentType<FormMiddlewareProps>[];
-    data?: any;
-    onChange?: (data: any) => void;
+    data?: unknown;
+    onChange?: (data: unknown) => void;
 }
-export declare function bindChildProps(props: FormMiddlewareProps): ((key: string | number) => FormMiddlewareProps) | null;
-export declare function useAdditional(props: FormMiddlewareProps, AdditionalItemTemplate: React.ComponentType<AdditionalItemTemplateProps> | null): UseAdditional;
 export declare function toJSONSchemaPath(dataPath: (string | number)[]): string;
 export declare function isRequired({ parent, dataPath }: FormMiddlewareProps): boolean;
-export declare const FixedObjectArrayMw: React.FC<FormMiddlewareProps>;
+export declare const FixedObjectMw: React.FC<FormMiddlewareProps>;
+export declare const FixedArrayMw: React.FC<FormMiddlewareProps>;
+export declare const LocalRefMw: React.FC<FormMiddlewareProps>;
+export declare const schemaMws: React.FC<FormMiddlewareProps<{}>>[];
 export declare const FormCore: React.FC<FormProps>;
 export default FormCore;
